2d4eee1c34d7b9e78a56608f1adf7e7200cbbb87
TRUST\TrustTheBoy <603049583@qq.com>
2017-02-12 06:35:53 +0800

    修复内容区域不能滑动，内容显示不完整的问题

--- src/components/details/details.vue
+++ src/components/details/details.vue
@@ -1,18 +1,20 @@
 <template>
-	<div class="details" id="details" v-el:foods-wrapper>
-		<div v-if="dataStart" class="details-data">
-			<div class="details-time-axis">
-				<ul class="details-time-axis-rod" :style="{height: dataItem*50-35 + 'px'}">
-					<li :style="{top: position(item)}" class="details-time-axis-rod-spot" 
-					v-for="(item, index) in details.mcpasssite" num={{item+1}}></li>
-				</ul>
-			</div>
-			<div class="details-channel-site">
-				<ul>
-					<li v-for="item in details.mcpasssite" class="details-channel-site-item" >
-						<span class="site-item-text">{{item.passsite}}</span>
-					</li>
-				</ul>
+	<div class="details" id="details" :style="{top: positionTop()}">
+		<div v-el:details-wrapper v-if="dataStart" class="box">
+			<div class="details-data">
+				<div class="details-time-axis">
+					<ul class="details-time-axis-rod" :style="{height: dataItem*50-35 + 'px'}">
+						<li :style="{top: position(item)}" class="details-time-axis-rod-spot" 
+						v-for="(item, index) in details.mcpasssite" num={{item+1}}></li>
+					</ul>
+				</div>
+				<div class="details-channel-site">
+					<ul>
+						<li v-for="item in details.mcpasssite" class="details-channel-site-item" >
+							<span class="site-item-text">{{item.passsite}}</span>
+						</li>
+					</ul>
+				</div>			
 			</div>			
 		</div>
 		<div v-else class="data-null">
@@ -28,47 +30,50 @@ export default {
 			details: [],
 			dataItem: 0,
 			dataStart: true,
-			spot: 0
+			spot: 0,
+			showBeizhu: false
 		}
 	},
-	methods: {
-		position(p) {
-			return (p * 50) + 'px'
-		}/*,
-		_initScroll() {
-				this.meunScroll = new BScroll(this.$els.menuWrapper, {
-					click: truen
-				});
-				this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
-					probeType: 3	// 监测滚动位置
-				});
-				this.foodsScroll.on('scroll', pos => {
-					this.scrollY = Math.abs(Math.round(pos.y));
-				});
-			}*/
-	},
 	created() {
+		this.$nextTick(() => {
+			this.showBeizhu = this.$parent.showBeizhu;
+		});
 		let test = '/api/vacation';		
 		let id = this.$route.params.id;
 		let url = "http://api.biaoxintong.com:8080/landing-craft/busSiteApiController.do?busbypass&lineid="+id;
 		this.$http.get(url).then(response => {		
 			let data = JSON.parse(response.data);
-			//this.details = data.details;
-			//this.dataItem=data.details.mcpasssite.length;
-			console.log(data.details.mcpasssite[0]);
 			if (data.details.mcpasssite[0].passsite =='0') {
 				this.dataStart = false;
 				return;
 			}
 			this.details = data.details;
-			this.dataItem=data.details.mcpasssite.length;	
+			this.dataItem = data.details.mcpasssite.length;	
 			this.dataStart = true;
-			//new BScroll(document.getElementById('details'));
-			/*this.$nextTick(() => {
+			this.$nextTick(() => {
 				this._initScroll();
-				//this._calculateHeight();
-			});*/
+			});
 		});
+	},
+	methods: {
+		position(p) {
+			return (p * 50) + 'px'
+		},
+		positionTop() {
+			if (!this.showBeizhu) {
+				return 100 + 'px'
+			}
+			return 133 + 'px'
+		},
+		positionBottom() {
+			if (!this.showBeizhu) {
+				return -33 + 'px'
+			}
+			return 0 + 'px'
+		},
+		_initScroll() {
+			this.detailsScroll = new BScroll(this.$els.detailsWrapper, {});
+		}
 	}
 }
 </script>
@@ -76,31 +81,37 @@ export default {
 @import "../../common/stylus/mixin";
 	.details
 		position: absolute
+		display: flex
+		bottom: 0
 		width: 100%
-		//top: 115px
-		//background-color: #fff
-		.details-data
-			display: flex
-			background-color: #fff
-			.details-time-axis
-				flex: 0 0 80px
-				width: 80px
-				padding-top: 20px
-				.details-time-axis-rod
-					width: 4px
-					background-color: #51A6FF
-					margin: 0 auto
-					position: relative
-					top: 0px
-					.details-time-axis-rod-spot
-						width: 15px
-						height: 15px
-						border-radius: 15px
+		overflow: hidden
+		.box
+			flex: 1
+			width: 100%
+			background: #f3f5f7
+			.details-data
+				display: flex
+				overflow: hidden
+				background-color: #fff
+				.details-time-axis
+					flex: 0 0 80px
+					width: 80px
+					padding-top: 20px
+					.details-time-axis-rod
+						width: 4px
 						background-color: #51A6FF
-						//background-color: rgba(10,10,20,0.2)
-						position: absolute
-						z-index: 2
-						left: -5.75px
+						margin: 0 auto
+						position: relative
+						top: 0px
+						.details-time-axis-rod-spot
+							width: 15px
+							height: 15px
+							border-radius: 15px
+							background-color: #51A6FF
+							//background-color: rgba(10,10,20,0.2)
+							position: absolute
+							z-index: 2
+							left: -5.75px
 			.details-channel-site
 				flex: 1
 				.details-channel-site-item
@@ -115,5 +126,5 @@ export default {
 					.site-item-text
 						font-size: 15px
 						color: #666
-		
+			
 </style>
--- src/components/weekend/weekend.vue
+++ src/components/weekend/weekend.vue
@@ -1,22 +1,24 @@
 <template>
-	<div class="weekend">
-		<ul v-if="dataStart">
-			<li v-for="item in weekend.working" class="nav-item nav-item-bg" >
-				<div class="nav-item-start">
-					<div class="nav-start"></div>
-				</div>
-				<div class="nav-item-details">
-					<div class="nav-text nav-item-text">
-						<span class="nav-text nav-text-start">{{item.mcorder}}</span>
-						<span class="nav-text nav-text-start">{{item.mcstarttime}}</span>
+	<div class="weekend" id="weekend" :style="{top: positionTop()}">
+		<div class="box" v-el:working-wrapper v-if="dataStart">	
+			<ul >
+				<li v-for="item in weekend.working" class="nav-item nav-item-bg" >
+					<div class="nav-item-start">
+						<div class="nav-start"></div>
 					</div>
-					<div class="nav-text-details-box">
-						<span class="nav-text nav-text-plate">车牌：{{item.mcnum}}</span>
-						<span class="nav-text nav-text-plate">司机：{{item.mclinkman}}</span>
+					<div class="nav-item-details">
+						<div class="nav-text nav-item-text">
+							<span class="nav-text nav-text-start">{{item.mcorder}}</span>
+							<span class="nav-text nav-text-start">{{item.mcstarttime}}</span>
+						</div>
+						<div class="nav-text-details-box">
+							<span class="nav-text nav-text-plate">车牌：{{item.mcnum}}</span>
+							<span class="nav-text nav-text-plate">司机：{{item.mclinkman}}</span>
+						</div>
 					</div>
-				</div>
-			</li>
-		</ul>
+				</li>
+			</ul>
+		</div>
 		<div v-else class="data-null">
 			暂无班车信息
 		</div>
@@ -33,10 +35,14 @@ export default {
 	data() {
 		return {
 			weekend: [],
-			dataStart: true
+			dataStart: true,
+			showBeizhu: false
 		}
 	},
 	created() {
+		this.$nextTick(() => {
+			this.showBeizhu = this.$parent.showBeizhu;
+		});
 		let test = '/api/weekend';
 		let id = this.$route.params.id;
 		let url = "http://api.biaoxintong.com:8080/landing-craft/busOrderApiController.do?weekend&lineid="+id;
@@ -48,7 +54,21 @@ export default {
 			}
 			this.weekend = data;	
 			this.dataStart = true;
+			this.$nextTick(() => {
+				this._initScroll();
+			});
 		});
+	},
+	methods: {
+		positionTop() {
+			if (!this.showBeizhu) {
+				return (133 - 33) + 'px'
+			}
+			return 133 + 'px'
+		},
+		_initScroll() {
+			this.workingScroll = new BScroll(this.$els.workingWrapper, {});
+		}
 	}
 
 }
@@ -57,10 +77,15 @@ export default {
 @import "../../common/stylus/mixin";
 .weekend	
 	position: absolute
+	display: flex
+	bottom: 0px
 	width: 100%
-	//top: 115px
-	height: 100%
-	//background-color: #fff
-	.nav-item-bg
-		background-color: #fff			
+	overflow: hidden
+	color: #2c3e50
+	.box
+		flex: 1
+		width: 100%
+		background: #f3f5f7		
+		.nav-item-bg
+			background-color: #fff			
 </style>
